8f02a9fc0c73d389586740a89b89d34a44f643a0
Thanh Nguyen <thanhqng1510@gmail.com>
2021-01-03 13:32:39 +0700

    Basic app

---
+++ app.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const morgan = require('morgan')
+const path = require('path')
+// require('express-async-errors')
+
+
+const app = express()
+
+app.use(morgan('dev'))
+app.use(express.urlencoded({
+	extended: true
+}))
+app.use(express.static(path.join(__dirname, 'public')))
+
+// require('./middlewares/session.mdw')(app)
+// require('./middlewares/view.mdw')(app)
+// require('./middlewares/locals.mdw')(app)
+// require('./middlewares/routes.mdw')(app)
+//
+// app.use(function (err, req, res, next) {
+// 	console.error(err.stack)
+// 	res.render('500', {
+// 		layout: false
+// 	})
+// })
+
+app.get('/', (req, res) => {
+	res.send('Hello World')
+})
+
+const PORT = process.env.PORT || 3000
+app.listen(PORT, function () {
+	console.log(`Educational Web App is listening on port ${PORT}`)
+})
