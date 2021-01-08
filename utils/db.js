f49da083cf7964521ab6f369fedbf907e25440ed
Thanh Nguyen <thanhqng1510@gmail.com>
2021-01-08 23:21:20 +0700

    Add .env for developing

--- utils/db.js
+++ utils/db.js
@@ -1,5 +1,12 @@
 const mysql = require('mysql2')
-const mysqlOpts = require('../config/default.json').mysql
+
+
+const mysqlOpts = {
+	user: process.env.DB_USER,
+	password: process.env.DB_PASSWORD,
+	database: process.env.DB_DB,
+	connectionLimit: process.env.DB_CONNECTION_LIMIT
+}
 
 
 const promisePool = mysql.createPool(mysqlOpts).promise()
